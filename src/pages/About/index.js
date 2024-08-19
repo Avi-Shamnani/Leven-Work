306f544ccc73b462e3cca2d50ba3c2c0ecdf0b50
abhaypandit03 <at9399131958@gmail.com>
2024-08-19 09:00:14 +0530

    slider added by Abhay

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import './about.css'
+
 
 
 // Layout is the fixed component in our page so we have wrapped all the other function inside it.
@@ -7,7 +9,40 @@ import Layout from '../../components/Layout'
   
 function About() {
   return (
-    <Layout><div>About</div></Layout>
+    <div class="about-page">
+    <Layout>
+      <section id="Heading">
+        <div id="head">
+          <h2><span>Wel</span>come to <span>L</span>EVEN <span>W</span>ORK</h2>
+
+        </div>
+      </section>
+    <section>
+      <div class="slider">
+        <div class="slide-track">
+          <div class="slide">
+            <img src="image/Img1.png" alt="Image 1" />
+          </div>
+          <div class="slide">
+            <img src="image/Img2.png" alt="Image 2" />
+          </div>
+          <div class="slide">
+            <img src="image/Img3.png" alt="Image 3" />
+          </div>
+          <div class="slide">
+            <img src="image/Img1.png" alt="Image 1" />
+          </div>
+          <div class="slide">
+            <img src="image/Img2.png" alt="Image 2" />
+          </div>
+          <div class="slide">
+            <img src="image/Img3.png" alt="Image 3" />
+          </div>
+        </div>
+      </div>
+    </section>
+    </Layout>
+    </div>
   )
 }
 
