eb9f2cec5b8cd814f56f7f6dd130921c28ac0051
Bhoomi Agrawal <bhoomiagrawal665@gmail.com>
2024-08-19 14:09:20 +0530

    adding the services page with very few content on our client page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Layout from '../../components/Layout';
 import './Services.css'; // Import your CSS file
 
+
 // Define the Home component
 function Home() {
   // Define the initial state for image index for each section
@@ -22,24 +23,29 @@ function Home() {
 
   // Effect for switching images when hovering
   useEffect(() => {
-    let intervalId;
+    const intervalIds = {};
+  
     const imageContainers = document.querySelectorAll('.b-image');
-
+  
     imageContainers.forEach((imageContainer, index) => {
       const images = imageContainer.querySelectorAll('img');
       const section = `section${index + 1}`;
+  
       imageContainer.addEventListener('mouseover', () => {
-        intervalId = setInterval(() => {
+        intervalIds[section] = setInterval(() => {
           switchImage(section, images.length);
         }, 2500);
       });
-
+  
       imageContainer.addEventListener('mouseout', () => {
-        clearInterval(intervalId);
+        clearInterval(intervalIds[section]);
+        delete intervalIds[section];
       });
     });
-
-    return () => clearInterval(intervalId);
+  
+    return () => {
+      Object.values(intervalIds).forEach((intervalId) => clearInterval(intervalId));
+    };
   }, []);
 
   // Render the Home component
@@ -49,21 +55,13 @@ function Home() {
 
         <div className="top-container">
           <div className="left-section">
-            <h1 style={{ fontSize: '65px', fontWeight: 'bold' }}>Our <br /> Services</h1>
+            <h1 style={{ fontSize: '65px', fontWeight: 'bold' }}>Our <br /> Ser<span class="blue">vi</span>ces</h1>
           </div>
           <div className="right-section">
-            <div className="curved-shape">
-            Welcome to our full-service digital agency, where creativity meets technology to drive
-                    your business forward. We specialize in a comprehensive range of services designed to
-                    elevate your brand in the digital landscape. From Digital Marketing strategies like SEO,
-                    PPC, and Social Media Marketing that increase your online visibility, to Creative Services
-                    including Graphic Design, Logo Design, and Video Production that breathe life into your
-                    brand identity. Our Web Development expertise ensures your online presence is not only
-                    aesthetically pleasing but also functional, with offerings like Website Design, E-commerce
-                    Solutions, and Web Analytics. Additionally, our Content Creation team crafts compelling
-                    narratives through Blog Writing, Copywriting, and Video Scriptwriting, ensuring your message
-                    resonates across all platforms. Partner with us to transform your ideas into impactful digital
-                    experiences.
+            <div className="curved-shape" style={{fontSize: '20px'}}>
+            Welcome to our <b>digital agency </b>, where innovation meets execution to elevate your brand. 
+                    We craft impactful strategies, stunning visuals, and seamless online experiences that resonate. 
+                    Partner with us to turn your vision into powerful digital realities.
             </div>
           </div>
         </div>
@@ -78,8 +76,8 @@ function Home() {
               {/* Add more images as needed */}
             </div>
             <div className="bs1-right b-text">
-              <h2 className='mt-10'>Digital Marketing :</h2>
-              <pre>
+              <h2 className="mt-10" style={{ fontSize: '35px', fontWeight: 'bold' }}>Digital Marketing :</h2>
+              <pre >
                 - SEO (Search Engine Optimization) <br />
                 - PPC (Pay-Per-Click) Advertising <br />
                 - Social Media Marketing <br />
@@ -92,7 +90,7 @@ function Home() {
           {/** Section 2 **/}
           <div className="bottom-section-2">
             <div className="bs2-left b-text">
-              <h2 className='mt-28'>Creative Services :</h2>
+              <h2 className="mt-10" style={{ fontSize: '35px', fontWeight: 'bold' }}>Creative Services :</h2>
               <pre>
                 - Graphic Design <br />
                 - Logo Design <br />
@@ -101,9 +99,12 @@ function Home() {
                 - Photography
               </pre>
             </div>
-            <div className="bs2-right b-image mt-0" data-hover-text="Creative Services">
+            <div className="bs2-right b-image" data-hover-text="Creative Services">
               <img src={`./images/image2.avif`} alt="pic 1" className="fixed-size-image" />
-              <img src={`images/image21.webp`} alt="pic 2" className="fixed-size-image" style={{ display: imageIndex.section2 === 1 ? 'block' : 'none' }} />
+              <img src={`./images/image21.webp`} alt="pic 2" className="fixed-size-image" style={{ display: imageIndex.section2 === 1 ? 'block' : 'none' }} />
+              <img src={`./images/image22.jpg`} alt="pic 3" className="fixed-size-image" style={{ display: imageIndex.section2 === 2 ? 'block' : 'none' }} />
+              <img src={`./images/image23.jpg`} alt="pic 4" className="fixed-size-image" style={{ display: imageIndex.section2 === 3 ? 'block' : 'none' }} />
+              <img src={`./images/image24.jpg`} alt="pic 5" className="fixed-size-image" style={{ display: imageIndex.section2 === 4 ? 'block' : 'none' }} />
               {/* Add more images as needed */}
             </div>
           </div>
@@ -111,13 +112,14 @@ function Home() {
           {/** Section 3 **/}
           <div className="bottom-section-3">
             <div className="bs3-left b-image" data-hover-text="Web Development">
-              <img src={`images/image3.png`} alt="pic 1" className="fixed-size-image" />
-              <img src={`images/image31.png`} alt="pic 2" className="fixed-size-image" style={{ display: imageIndex.section3 === 1 ? 'block' : 'none' }} />
+              <img src={`./images/image3.png`} alt="pic 1" className="fixed-size-image" />
+              <img src={`./images/image31.png`} alt="pic 2" className="fixed-size-image" style={{ display: imageIndex.section3 === 1 ? 'block' : 'none' }} />
+              <img src={`./images/image32.png`} alt="pic 3" className="fixed-size-image" style={{ display: imageIndex.section2 === 2 ? 'block' : 'none' }} />
               {/* Add more images as needed */}
             </div>
             <div className="bs3-right b-text">
-              <h2 className='mt-10'>Web Development :</h2>
-              <pre>
+              <h2 className="mt-10" style={{ fontSize: '35px', fontWeight: 'bold' }}>Web Development :</h2>
+              <pre >
                 - Website Design <br />
                 - Website Development <br />
                 - E-commerce Solutions <br />
@@ -130,8 +132,8 @@ function Home() {
           {/** Section 4 **/}
           <div className="bottom-section-4">
             <div className="bs4-left b-text">
-              <h2 className='mt-20'>Content Creation :</h2>
-              <pre>
+              <h2 className="mt-10" style={{ fontSize: '35px', fontWeight: 'bold' }}>Content Creation :</h2>
+              <pre >
                 - Blog Writing <br />
                 - Copywriting <br />
                 - Video Scriptwriting <br />
@@ -140,12 +142,18 @@ function Home() {
               </pre>
             </div>
             <div className="bs4-right b-image" data-hover-text="Content Creation">
-              <img src={`images/image4.png`} alt="pic 1" className="fixed-size-image" />
-              <img src={`images/image41.png`} alt="pic 2" className="fixed-size-image" style={{ display: imageIndex.section4 === 1 ? 'block' : 'none' }} />
+              <img src={`./images/image4.png`} alt="pic 1" className="fixed-size-image" />
+              <img src={`./images/image41.png`} alt="pic 2" className="fixed-size-image" style={{ display: imageIndex.section4 === 1 ? 'block' : 'none' }} />
               {/* Add more images as needed */}
             </div>
           </div>
         </div>
+
+        <div className='footer'>
+
+        </div>
+
+        
       </div>
     </Layout>
   );
