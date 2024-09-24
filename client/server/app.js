62a824811825dcfb1ca4559b3eb32d34a211548f
amanurmaliya <amanurmaliya609@gmail.com>
2024-09-24 17:35:45 +0530

    final

---
+++ client/server/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const clientModel = require("./models/client.model.js");
+
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+
+app.post("/contact", async (req, res) => {
+  console.log(req.body);
+  try {
+    const { name, email, phone, business, services, location, description } =
+      req.body;
+    const client = new clientModel({
+      name,
+      email,
+      phone,
+      business,
+      services,
+      location,
+      description,
+    });
+    await client.save();
+    res.status(201).send({ message: "Client data saved successfully!" });
+  } catch (error) {
+    console.error("Failed to save client data:", error);
+    res.status(500).send({ error: "Failed to save client data" });
+  }
+});
+
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
